550bba5eee83f57aecdee98b646e9a89f706ec30
janvisharma <janvi@connect.hku.hk>
2020-03-14 14:05:23 +0530

    changes sls image and color schemes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import TeamMember from './components/TeamMember';
-import Header from './components/Header'
+
 import SubHeading from './components/SubHeading'
 import ProjectCard from './components/ProjectCard';
 import NavBar from './components/NavBar';
