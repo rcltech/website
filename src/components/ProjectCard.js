0be727e2d23722b0f7d3caeb143196ef6172ac38
janvisharma <janvi@connect.hku.hk>
2020-03-26 02:52:58 +0530

    changes to make website responsive

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -3,14 +3,16 @@ import './ProjectCard.css';
 
 const ProjectCard = (props) => {
     return(
-        <div className='ProjectCard'>
-            <div className='ProjectCardTop'>
-            <a className="text-link" target="_blank" href={props.link}><img alt="project card" src={require('./Images/'+props.url+'.png')} /></a>
-            </div>
-            <div className='ProjectCardBottom'>
-                <h1>{props.projectName}</h1>
-                <h3>{props.name}</h3>
-                <p>{props.about}</p>
+        <div className='ProjectCard'> 
+            <div className='Row'>
+                <div className='ProjectCardImg'>
+                <a className="text-link" target="_blank" href={props.link}><img alt="project card" src={require('./Images/'+props.url+'.png')} /></a>
+                </div>
+                <div className='ProjectCardInfo'>
+                    <h1>{props.projectName}</h1>
+                    <h3>{props.name}</h3>
+                    <p>{props.about}</p>
+                </div>
             </div>
         </div>
     );
